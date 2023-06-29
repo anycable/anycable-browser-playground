cf1a2c8a3d7e111df6a2e19be44fe1c5605e56cb
Vladimir Dementyev <palkan@hey.com>
2023-06-28 19:21:22 -0700

    Add EventTarget polyfill

--- fetch.es5.js
+++ fetch.es5.js
@@ -1,2 +1,6 @@
+import EventTargetPolyfill from "@mattkrick/event-target-polyfill";
+window.EventTargetPolyfill = EventTargetPolyfill;
+window.EventTarget = EventTargetPolyfill;
+
 import "yet-another-abortcontroller-polyfill";
 import "whatwg-fetch";
